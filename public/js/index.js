9082696465195afad22a852f99411b6c581bcbbe
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-11-11 22:44:14 -0600

    added axios for request handling

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,13 @@
 const API = {
     getData: function() {
         console.log("hello?");
-        $.ajax({
-            url: "http://13.58.61.47:5000/api/v1/items/ranked?emphasized=profit,profit%20percent&history_region_id=Etherium_Reach_10000027",
+       return $.ajax({
+            url: "/api/profit-table",
             type: "GET",
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-        .done((response)=> {
-            let data = JSON.parse(response);
-            console.log(data[0]);
-        });
-
     }
 }
 
--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -1,15 +1,19 @@
+const { default: Axios } = require("axios");
+
+const axios = require('axios');
+
 module.exports = function(app) {
-    app.get("/api/table", function(req, res){
+
+    app.get("/api/profit-table", function(req, res){
         let apiInfo = {
-		url: "http://13.58.61.47:5000/api/v1/items/ranked?emphasized=profit,profit%20percent&history_region_id=Etherium_Reach_10000027", //URL to external API for data
+            url: "http://13.58.61.47:5000/api/v1/items/ranked?emphasized=profit,profit%20percent&history_region_id=Etherium_Reach_10000027", //URL to external API for data
             method: "GET"
-            // headers: {
         }
 
-        $.ajax(apiInfo)
-        .done((response)=> {
+        axios.get(apiInfo.url)
+        .then((response)=> {
             console.log(response);
-            // populate table here
+            res.json(response);
         });
     });
 
