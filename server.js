8565847f943ef0c5d32c0c0958cb62e371973d67
Calvin Freese <calvin.p.freese@gmail.com>
2021-02-11 18:59:47 -0600

    updated server.js (removed handlebars info), added in proxy to localhost:8080 in clientside

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 const express = require('express');
-const axios = require('axios');
 const cors = require('cors');
-let expHandleBars = require('express-handlebars');
 
 
-let PORT = process.env.PORT || 8080;
-let app = express();
 
-app.use(express.static("./public"));
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// ---- handlebars = html injection ----
-app.engine('handlebars', expHandleBars({ defaultLayout: "main" }));
-app.set('view engine', 'handlebars');
 
 // ---- page routing & api routing ---- 
-require("./server/routes/html-routes.js")(app);
-require("./server/routes/api-routes.js")(app);
+require("./routes/api")(app);
 
 app.listen(PORT, function () {
     console.log("Server listening on http://localhost:" + PORT);
