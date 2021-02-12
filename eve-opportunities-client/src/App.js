e133e35f016394b5a6a29865b45b5456646d6da9
Calvin Freese <calvin.p.freese@gmail.com>
2021-02-11 19:34:19 -0600

    added materialUI components

--- eve-opportunities-client/src/App.js
+++ eve-opportunities-client/src/App.js
@@ -1,23 +1,15 @@
+import { Switch, Route } from "react-router-dom"
+import Dashboard from "./components/Dashboard.js";
+import Homepage from "./pages/Homepage";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+      </Switch>
     </div>
   );
 }
