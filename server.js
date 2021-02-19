75074176d8c8f7ab60cfe77490cd0021bd37baa8
mobone <nicholas.reichel@gmail.com>
2021-02-19 02:24:09 -0600

    testing bind

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 // ---- page routing & api routing ----
 require("./routes/api")(app);
 
-app.listen(PORT, function () {
-    console.log("Server listening on http://localhost:" + PORT);
-});
+//app.listen(PORT, function () {
+  //  console.log("Server listening on http://localhost:" + PORT);
+//});
+app.listen(3000, '127.0.0.1');
